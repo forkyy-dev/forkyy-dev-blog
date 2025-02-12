864f8e14ab13866bf357ab1a5c46872ccfb4b1de
forkyy-dev <forkyy.dev@gmail.com>
2025-02-12 17:40:25 +0900

    blog initial setting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,13 @@
 require("katex/dist/katex.min.css")
+
+exports.onRouteUpdate = ({ location }) => {
+  if (typeof window !== "undefined") {
+    if (window.location.pathname.startsWith("/forkyy-dev/")) {
+      window.history.replaceState(
+        {},
+        "",
+        window.location.pathname.replace("/forkyy-dev", "")
+      )
+    }
+  }
+}
